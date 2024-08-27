18cef2c41258f140a1052dc766352dd3c861fb41
trvhung <hunq281@gmail.com>
2024-08-27 17:45:22 +0700

    a

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -77,7 +77,7 @@
     {$t('footer.copyright')}
   </p>
   <div class="flex mt-4 md:items-center flex-col md:flex-row">
-    <a class="text-gray-400 hover:text-primary" href="https://discord.gg/tPURAYgHV9" target="_blank">
+    <a class="text-gray-400 hover:text-primary" href="https://discord.com/" rel="nofollow" target="_blank">
       <Icon path={mdiDiscord} size={1.5} />
       {$t('footer.discord')}
     </a>
@@ -86,7 +86,7 @@
       <div>
         <a
           class="text-gray-400 hover:text-primary whitespace-nowrap"
-          href="https://github.com/MadeBaruna/paimon-moe"
+          href=""
           target="_blank"
         >
           <Icon path={mdiGithub} size={1} />
@@ -94,7 +94,7 @@
         </a>
         <a
           class="text-gray-400 hover:text-primary whitespace-nowrap"
-          href="https://twitter.com/MadeBaruna"
+          href=""
           target="_blank"
         >
           <Icon path={mdiTwitter} size={1} />
@@ -104,32 +104,18 @@
     </div>
     <div class="text-gray-400 mt-4 md:mt-0 md:ml-4 flex flex-col md:pl-4 md:border-l border-gray-600">
       <span class="text-gray-500">{$t('footer.official')}</span>
-      <div>
-        <a
-          class="text-gray-400 hover:text-primary mr-1 whitespace-nowrap"
-          href="https://discord.gg/4nbWsCGjjE"
-          target="_blank"
-        >
-          <Icon path={mdiDiscord} size={1} />
-          {$t('footer.link.discord')}
-        </a>
-        <a
-          class="text-gray-400 hover:text-primary mr-1 whitespace-nowrap"
-          href="https://www.facebook.com/Genshinimpact/"
-          target="_blank"
-        >
-          <Icon path={mdiFacebook} size={1} />
-          {$t('footer.link.facebook')}
-        </a>
-        <a
-          class="text-gray-400 hover:text-primary whitespace-nowrap"
-          href="https://www.reddit.com/r/Genshin_Impact/"
-          target="_blank"
-        >
-          <Icon path={mdiReddit} size={1} />
-          {$t('footer.link.reddit')}
-        </a>
-      </div>
+      <svg
+      width="36"
+      height="36"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      class="fill-current">
+      <path
+        d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+    </svg>
+    <p>Special thank for  Paimon.Moe</p>
     </div>
     <div
       class="text-gray-400 mt-4 md:mt-0 md:ml-4 flex flex-col justify-center h-full md:pl-4 md:border-l border-gray-600"
